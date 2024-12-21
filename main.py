d380c9bbff662b3eb52cb56054a083eca7ce20f1
spawlov <107248409+spawlov@users.noreply.github.com>
2024-12-21 12:39:39 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import urllib.parse
+
+import requests
+
+
+def get_weather_report(place: str) -> str:
+    template_url = f"https://wttr.in/{place}?nTqu&lang=ru"
+    encoded_place = urllib.parse.quote(place)
+    url = template_url.replace("{place}", encoded_place)
+    response = requests.get(
+        url=url,
+        timeout=30,
+    )
+    response.raise_for_status()
+    return response.text
+
+
+def main(places: list[str]) -> None:
+    for place in places:
+        print(get_weather_report(place))  # noqa
+
+
+if __name__ == "__main__":
+    main(["Москва", "Шереметьево", "Череповец"])
